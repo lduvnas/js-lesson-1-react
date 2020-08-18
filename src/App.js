9648a2f01816d80cf2eeb17d25034f557799a29a
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-08-18 15:31:06 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import solar from "./solar.svg";
+import "./App.css";
+import MyButton from "./components/MyButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={solar} className="App-logo" alt="solar" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Js Lesson 1
+          <MyButton title="tryck här" />
+          <MyButton title="eller här" />
+          <MyButton title="varför inte här?" />
+        </p>
+
+        <p>
+          <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
